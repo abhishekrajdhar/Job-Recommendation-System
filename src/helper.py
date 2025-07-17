43cf4ba1b01881ae30652d2464d2a2c79a300b3f
abhishekrajdhar <rajdhardubey4434@gmail.com>
2025-07-17 18:29:22 +0530

    added streamlit app

--- src/helper.py
+++ src/helper.py
@@ -13,6 +13,24 @@ os.environ["GEMINI_API_KEY"] = GEMINI_API_KEY
 client = genai.GenerativeModel(api_key=GEMINI_API_KEY)
 
 
+def extract_text_from_pdf(uploaded_file):
+    """
+    Extracts text from a PDF file.
+    
+    Args:
+        uploaded_file (str): The path to the PDF file.
+        
+    Returns:
+        str: The extracted text.
+    """
+    doc = fitz.open(stream=uploaded_file.read(), filetype="pdf")
+    text = ""
+    for page in doc:
+        text += page.get_text()
+    return text
+
+
+
 def ask_genai(prompt, model_name="gemini-2.0-flash", temperature=0.5, max_tokens=500):
     """
     Sends a prompt to the Gemini (Google GenAI) model and returns the response.
